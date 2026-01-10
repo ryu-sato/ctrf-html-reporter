a35043a0eb01de2d3cdebeb67eac902a16de9187
Ryu Sato <ryu@weseek.co.jp>
2026-01-10 07:42:57 +0000

    Add ToolInfo component to display tool details in the sidebar

--- src/reporter/.vitepress/theme/layouts/Layout.vue
+++ src/reporter/.vitepress/theme/layouts/Layout.vue
@@ -76,7 +76,6 @@ provide(layoutInfoInjectionKey, { heroImageSlotExists })
         <div class="w-full mt-4 mb-4 border-t border-[var(--vp-c-tip-soft)]" />
         <h2 class="text-xl text-[var(--vp-c-text-3)]">Tool Details</h2>
         <ToolInfo :tool="report.results.tool" />
-
       </template>
     </VPSidebar>
 
